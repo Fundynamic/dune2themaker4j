7d82fd23c19ac7edd5f95d3e8c348cc57c22a4ce
Stefan Hendriks <stefanhen83@gmail.com>
2015-02-01 16:55:41 +0100

    found a bug with the TOP_LEFT test - redo implementation (I am bad at this bitmask stuff :))

--- src/test/java/com/fundynamic/d2tm/game/map/ShroudFacingDeterminerTest.java
+++ src/test/java/com/fundynamic/d2tm/game/map/ShroudFacingDeterminerTest.java
@@ -1,22 +1,34 @@
 package com.fundynamic.d2tm.game.map;
 
-import com.fundynamic.d2tm.graphics.ShroudFacing;
 import junit.framework.Assert;
 import org.junit.Test;
 
 import static com.fundynamic.d2tm.game.map.ShroudFacingDeterminer.getFacing;
+import static com.fundynamic.d2tm.graphics.ShroudFacing.*;
 
 public class ShroudFacingDeterminerTest {
 
     @Test
-    public void RIGHT_BOTTOM_LEFT() throws Exception {
-        Assert.assertEquals(ShroudFacing.RIGHT_BOTTOM_LEFT, getFacing(false, true, true, true));
+    public void FULL_is_null() throws Exception {
+        Assert.assertTrue(getFacing(true, true, true, true) != FULL);
+        Assert.assertEquals(null, getFacing(true, true, true, true)); // instead of FULL
     }
 
     @Test
     public void TOP_LEFT() throws Exception {
-        Assert.assertEquals(ShroudFacing.TOP_LEFT, getFacing(true, false, false, true));
+        Assert.assertEquals(TOP_LEFT, getFacing(true, false, false, true));
     }
+
+    @Test
+    public void TOP() throws Exception {
+        Assert.assertEquals(TOP, getFacing(true, false, false, false));
+    }
+
+    @Test
+    public void RIGHT_BOTTOM_LEFT() throws Exception {
+        Assert.assertEquals(RIGHT_BOTTOM_LEFT, getFacing(false, true, true, true));
+    }
+
 
     @Test
     public void returnsNullWhenNoShroudAround() throws Exception {
