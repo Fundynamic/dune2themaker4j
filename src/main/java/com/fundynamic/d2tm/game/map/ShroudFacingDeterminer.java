6b0696428abd29686c185e412bc798cf3df07f86
Stefan Hendriks <stefanhen83@gmail.com>
2015-02-01 17:04:48 +0100

    implement all other sides: - full is null because 'full' is logically insane to 'draw' (ie, that was the old logic). You would rather omit drawing, than draw everything and then draw a full black block over everything else again.

--- src/main/java/com/fundynamic/d2tm/game/map/ShroudFacingDeterminer.java
+++ src/main/java/com/fundynamic/d2tm/game/map/ShroudFacingDeterminer.java
@@ -7,6 +7,15 @@ public class ShroudFacingDeterminer {
     public static ShroudFacing getFacing(boolean isTopShrouded, boolean isRightShrouded, boolean isBottomShrouded, boolean isLeftShrouded) {
         if (isTopShrouded && isLeftShrouded && !isRightShrouded && !isBottomShrouded) return ShroudFacing.TOP_LEFT;
         if (isTopShrouded && !isLeftShrouded && !isRightShrouded && !isBottomShrouded) return ShroudFacing.TOP;
+        if (isTopShrouded && !isLeftShrouded && isRightShrouded && !isBottomShrouded) return ShroudFacing.TOP_RIGHT;
+        if (!isTopShrouded && !isLeftShrouded && isRightShrouded && !isBottomShrouded) return ShroudFacing.RIGHT;
+        if (!isTopShrouded && !isLeftShrouded && isRightShrouded && isBottomShrouded) return ShroudFacing.RIGHT_BOTTOM;
+        if (!isTopShrouded && !isLeftShrouded && !isRightShrouded && isBottomShrouded) return ShroudFacing.BOTTOM;
+        if (!isTopShrouded && isLeftShrouded && !isRightShrouded && isBottomShrouded) return ShroudFacing.BOTTOM_LEFT;
+        if (!isTopShrouded && isLeftShrouded && !isRightShrouded && !isBottomShrouded) return ShroudFacing.LEFT;
+        if (isTopShrouded && isLeftShrouded && !isRightShrouded && isBottomShrouded) return ShroudFacing.TOP_BOTTOM_LEFT;
+        if (isTopShrouded && isLeftShrouded && isRightShrouded && !isBottomShrouded) return ShroudFacing.TOP_RIGHT_LEFT;
+        if (isTopShrouded && !isLeftShrouded && isRightShrouded && isBottomShrouded) return ShroudFacing.TOP_RIGHT_BOTTOM;
         if (!isTopShrouded && isLeftShrouded && isRightShrouded && isBottomShrouded) return ShroudFacing.RIGHT_BOTTOM_LEFT;
         return null;
     }
--- src/test/java/com/fundynamic/d2tm/game/map/ShroudFacingDeterminerTest.java
+++ src/test/java/com/fundynamic/d2tm/game/map/ShroudFacingDeterminerTest.java
@@ -25,10 +25,54 @@ public class ShroudFacingDeterminerTest {
     }
 
     @Test
+    public void TOP_RIGHT() throws Exception {
+        Assert.assertEquals(TOP_RIGHT, getFacing(true, true, false, false));
+    }
+
+    @Test
+    public void RIGHT() throws Exception {
+        Assert.assertEquals(RIGHT, getFacing(false, true, false, false));
+    }
+
+    @Test
+    public void RIGHT_BOTTOM() throws Exception {
+        Assert.assertEquals(RIGHT_BOTTOM, getFacing(false, true, true, false));
+    }
+
+    @Test
+    public void BOTTOM() throws Exception {
+        Assert.assertEquals(BOTTOM, getFacing(false, false, true, false));
+    }
+
+    @Test
+    public void BOTTOM_LEFT() throws Exception {
+        Assert.assertEquals(BOTTOM_LEFT, getFacing(false, false, true, true));
+    }
+
+    @Test
+    public void LEFT() throws Exception {
+        Assert.assertEquals(LEFT, getFacing(false, false, false, true));
+    }
+
+    @Test
+    public void TOP_BOTTOM_LEFT() throws Exception {
+        Assert.assertEquals(TOP_BOTTOM_LEFT, getFacing(true, false, true, true));
+    }
+
+    @Test
+    public void TOP_RIGHT_LEFT() throws Exception {
+        Assert.assertEquals(TOP_RIGHT_LEFT, getFacing(true, true, false, true));
+    }
+
+    @Test
+    public void TOP_RIGHT_BOTTOM() throws Exception {
+        Assert.assertEquals(TOP_RIGHT_BOTTOM, getFacing(true, true, true, false));
+    }
+
+    @Test
     public void RIGHT_BOTTOM_LEFT() throws Exception {
         Assert.assertEquals(RIGHT_BOTTOM_LEFT, getFacing(false, true, true, true));
     }
-
 
     @Test
     public void returnsNullWhenNoShroudAround() throws Exception {
