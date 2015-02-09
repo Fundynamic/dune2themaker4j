0d2051a445a35f82555868a111a13233df85b463
Stefan Hendriks <stefanhen83@gmail.com>
2015-02-09 20:29:03 +0100

    remove obselete tests

--- src/test/java/com/fundynamic/d2tm/game/map/CellTest.java
+++ src/test/java/com/fundynamic/d2tm/game/map/CellTest.java
@@ -3,9 +3,6 @@ package com.fundynamic.d2tm.game.map;
 import com.fundynamic.d2tm.game.terrain.Terrain;
 import org.junit.Assert;
 import org.junit.Test;
-import org.mockito.Mockito;
-
-import static org.mockito.Matchers.any;
 
 public class CellTest {
 
@@ -13,22 +10,5 @@ public class CellTest {
     public void constructorThrowsIllegalArgumentExceptionWhenArgumentIsNull() {
         new Cell(null);
     }
-
-    @Test
-    public void isSameTerrainReturnsTrueWhenTerrainEquals() {
-        Terrain terrain = Mockito.mock(Terrain.class);
-        Mockito.when(terrain.isSame(any(Terrain.class))).thenReturn(true);
-
-        final Cell cell = new Cell(terrain);
-        Assert.assertTrue(cell.isSameTerrain(Mockito.mock(Terrain.class)));
-    }
-
-//
-//	@Test
-//	public void isSameTerrainReturnsFalseWhenClassNameDoesNotEqual() {
-//		final Cell cell = new Cell(new TestableSandTerrain());
-//		Assert.assertTrue(cell.isSameTerrain(new TestableSandTerrain()));
-//	}
-
 
 }
