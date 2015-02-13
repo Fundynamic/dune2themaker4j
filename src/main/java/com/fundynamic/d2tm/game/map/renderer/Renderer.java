554aa56bb76964ddc5c4b28442f418cb789af853
Stefan Hendriks <stefanhen83@gmail.com>
2015-02-13 21:57:13 +0100

    add documentation about the current idea.

--- src/main/java/com/fundynamic/d2tm/game/map/renderer/Renderer.java
+++ src/main/java/com/fundynamic/d2tm/game/map/renderer/Renderer.java
@@ -2,6 +2,25 @@ package com.fundynamic.d2tm.game.map.renderer;
 
 import org.newdawn.slick.Graphics;
 
+/**
+ * <p>
+ * Very abstract way of rendering 'things' on the screen.
+ * </p>
+ * <p>
+ * This is used by the {@link com.fundynamic.d2tm.game.map.renderer.ViewportRenderer} to draw actual stuff on the screen.
+ * </p>
+ * <p>
+ * A {@link com.fundynamic.d2tm.game.map.renderer.ViewportRenderer} is responsible for culling/selecting stuff to draw
+ * on the screen, but delegates *what* to draw to a concrete implementation of this Renderer. The types (T) must be
+ * the same for both ViewportRenderer and this Renderer.
+ * </p>
+ * <p>
+ * The drawX and drawY positions are the topLeft pixel absolute coordinates on the screen to render. This means that
+ * the Renderer implementations only need to concern themselves with game logic / animation.
+ * </p>
+ *
+ * @param <T> Type of thing to render
+ */
 interface Renderer<T> {
 
     void draw(Graphics graphics, T thingToRender, int drawX, int drawY);
--- src/main/java/com/fundynamic/d2tm/game/map/renderer/ViewportRenderer.java
+++ src/main/java/com/fundynamic/d2tm/game/map/renderer/ViewportRenderer.java
@@ -5,6 +5,20 @@ import com.fundynamic.d2tm.game.math.Vector2D;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 
+/**
+ * <p>
+ *     A ViewportRenderer is a high level abstraction of drawing stuff on the screen from a viewport perspective. *
+ * </p>
+ * <p>
+ *     Implementations of this interface will select things of type T on any source of data (Map, Structures list, etc).
+ *     When they are a candidate to draw on the actual screen it should delegate this to a concrete implementation of
+ *     {@link com.fundynamic.d2tm.game.map.renderer.Renderer} of same type T.
+ * </p>
+ * <p>
+ *     Example: Selecting which MapCells should be drawn is a different logic than selecting which Structures must be drawn
+ * </p>
+ * @param <T>
+ */
 public interface ViewportRenderer<T> {
 
     public void render(Image imageToDrawOn, Vector2D viewingVector, Renderer<T> renderer) throws SlickException;
