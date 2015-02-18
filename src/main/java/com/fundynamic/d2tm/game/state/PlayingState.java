514a723645735e379031dc6f5cd66c8afa4b1d7a
Stefan Hendriks <stefanhen83@gmail.com>
2015-02-18 21:24:31 +0100

    optimize import

--- src/main/java/com/fundynamic/d2tm/game/state/PlayingState.java
+++ src/main/java/com/fundynamic/d2tm/game/state/PlayingState.java
@@ -13,7 +13,6 @@ import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
-import org.newdawn.slick.geom.Vector2f;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
