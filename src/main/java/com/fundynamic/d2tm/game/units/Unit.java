abfc715eac3b696f60fba13d7163c451b8ee63da
Stefan Hendriks <stefanhen83@gmail.com>
2015-02-20 15:46:53 +0100

    no need to store height/width for unit

--- src/main/java/com/fundynamic/d2tm/game/units/Unit.java
+++ src/main/java/com/fundynamic/d2tm/game/units/Unit.java
@@ -8,19 +8,15 @@ import org.newdawn.slick.SpriteSheet;
 public class Unit {
     private final Vector2D mapCoordinates;
     private final SpriteSheet spriteSheet;
-    private final int width;
-    private final int height;
     private final int facing;
 
     public Unit(Vector2D mapCoordinates, Image imageOfStructure, int width, int height) {
-        this(mapCoordinates, new SpriteSheet(imageOfStructure, width, height), width, height);
+        this(mapCoordinates, new SpriteSheet(imageOfStructure, width, height));
     }
 
-    public Unit(Vector2D mapCoordinates, SpriteSheet spriteSheet, int width, int height) {
+    public Unit(Vector2D mapCoordinates, SpriteSheet spriteSheet) {
         this.mapCoordinates = mapCoordinates;
         this.spriteSheet = spriteSheet;
-        this.width = width;
-        this.height = height;
         int possibleFacings = spriteSheet.getHorizontalCount();
         this.facing = Random.getRandomBetween(0, possibleFacings);
     }
