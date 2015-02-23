6bfcd82715158e80114e9ea6b813f777993ffbdf
Stefan Hendriks <stefanhen83@gmail.com>
2015-02-23 20:53:40 +0100

    organise imports

--- src/main/java/com/fundynamic/d2tm/game/map/Map.java
+++ src/main/java/com/fundynamic/d2tm/game/map/Map.java
@@ -1,6 +1,5 @@
 package com.fundynamic.d2tm.game.map;
 
-import com.fundynamic.d2tm.game.entities.Entity;
 import com.fundynamic.d2tm.game.entities.structures.Structure;
 import com.fundynamic.d2tm.game.entities.units.Unit;
 import com.fundynamic.d2tm.game.terrain.Terrain;
@@ -8,9 +7,6 @@ import com.fundynamic.d2tm.game.terrain.TerrainFactory;
 import com.fundynamic.d2tm.graphics.Shroud;
 import com.fundynamic.d2tm.math.Vector2D;
 import org.newdawn.slick.SlickException;
-
-import java.util.HashSet;
-import java.util.Set;
 
 public class Map {
 
