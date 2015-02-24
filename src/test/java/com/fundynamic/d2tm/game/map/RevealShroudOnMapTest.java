7f43400139aae8fa06d169c29e966d4cc679dc49
Stefan Hendriks <stefanhen83@gmail.com>
2015-02-24 20:36:34 +0100

    reveal one cell

--- src/test/java/com/fundynamic/d2tm/game/map/RevealShroudOnMapTest.java
+++ src/test/java/com/fundynamic/d2tm/game/map/RevealShroudOnMapTest.java
@@ -73,4 +73,20 @@ public class RevealShroudOnMapTest {
                 map.getAsciiShroudMap());
     }
 
+    @Test
+    public void revealShroudForWithOnlyXAndYRevealsOneCellOnly() {
+        map.revealShroudFor(5, 5); // this is 0 based, so it is the 6th row!
+        Assert.assertEquals(
+                "##########\n" + // 1
+                "##########\n" + // 2
+                "##########\n" + // 3
+                "##########\n" + // 4
+                "##########\n" + // 5
+                "#####.####\n" + // 6
+                "##########\n" + // 7
+                "##########\n" + // 8
+                "##########\n" + // 9
+                "##########\n",  // 10
+                map.getAsciiShroudMap());
+    }
 }
