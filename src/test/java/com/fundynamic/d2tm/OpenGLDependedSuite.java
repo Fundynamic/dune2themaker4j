338b0601ca5df7b81de07bfcf296c24b524b351e
Stefan Hendriks <stefanhen83@gmail.com>
2015-03-08 20:26:24 +0100

    thank you gitx

--- src/test/java/com/fundynamic/d2tm/OpenGLDependedSuite.java
+++ src/test/java/com/fundynamic/d2tm/OpenGLDependedSuite.java
@@ -1,7 +1,30 @@
 package com.fundynamic.d2tm;
 
-/**
- * Created by shendriks on 08/03/15.
- */
+
+import com.fundynamic.d2tm.game.entities.EntityRepositoryTest;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
+//@RunWith(Suite.class)
+//@SuiteClasses({EntityRepositoryTest.class})
 public class OpenGLDependedSuite {
+
+    @BeforeClass
+    public static void setUp() throws LWJGLException {
+        Display.setDisplayMode(new DisplayMode(800, 600));
+        Display.setVSyncEnabled(true);
+        Display.setTitle("d2tm unit test");
+        Display.create();
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        Display.destroy();
+    }
 }
