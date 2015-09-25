72727ecd7cb8e4d07b446da1c52450d102d74e5d
Stefan Hendriks <stefan@fundynamic.com>
2015-09-25 17:21:05 +0200

    .gitignore also ignored this file, force add

---
+++ src/main/java/com/fundynamic/d2tm/graphics/CannotLoadImageException.java
@@ -0,0 +1,10 @@
+package com.fundynamic.d2tm.graphics;
+
+
+public class CannotLoadImageException extends RuntimeException {
+
+    public CannotLoadImageException(Throwable cause) {
+        super(cause);
+    }
+
+}
