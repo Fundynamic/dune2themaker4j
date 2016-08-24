5867bfe0ad74abf60bbe2f126494d3491d3d9538
Stefan Hendriks <stefan@fundynamic.com>
2016-08-24 11:39:29 +0200

    Add new test

--- src/test/java/com/fundynamic/d2tm/game/rendering/gui/battlefield/RenderQueueTest.java
+++ src/test/java/com/fundynamic/d2tm/game/rendering/gui/battlefield/RenderQueueTest.java
@@ -1,8 +1,20 @@
-import static org.junit.Assert.*;
+package com.fundynamic.d2tm.game.rendering.gui.battlefield;
 
-/**
- * Created by shendriks on 24/08/16.
- */
-public class RenderQueueTest {
+import com.fundynamic.d2tm.game.AbstractD2TMTest;
+import com.fundynamic.d2tm.game.entities.NullEntity;
+import com.fundynamic.d2tm.math.Vector2D;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+
+public class RenderQueueTest extends AbstractD2TMTest {
+
+
+    @Test
+    public void putListOfEntities() {
+        RenderQueue renderQueue = new RenderQueue(Vector2D.create(10, 10));
+        renderQueue.put(Arrays.asList(new NullEntity()));
+    }
 
 }
