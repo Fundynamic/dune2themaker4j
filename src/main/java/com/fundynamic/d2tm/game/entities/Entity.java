d73d985e14af5b04f17bef995419620fd4e1ee29
Stefan Hendriks <stefan@fundynamic.com>
2017-08-04 09:26:35 +0200

    Rename, and hide

--- src/main/java/com/fundynamic/d2tm/game/entities/Entity.java
+++ src/main/java/com/fundynamic/d2tm/game/entities/Entity.java
@@ -228,11 +228,11 @@ public abstract class Entity implements EnrichableAbsoluteRenderable, Updateable
     }
 
     public List<MapCoordinate> getAllSurroundingCellsAsCoordinates() {
-        return getMapCoordinates(coordinate.toMapCoordinate()); // coordinate == top left
+        return getAllSurroundingCellsAsMapCoordinatesStartingFromTopLeft(coordinate.toMapCoordinate()); // coordinate == top left
     }
 
     // this basically goes 'around' the entity, starting from top-left
-    public List<MapCoordinate> getMapCoordinates(MapCoordinate topLeftMapCoordinate) {
+    private List<MapCoordinate> getAllSurroundingCellsAsMapCoordinatesStartingFromTopLeft(MapCoordinate topLeftMapCoordinate) {
         int currentX = topLeftMapCoordinate.getXAsInt();
         int currentY = topLeftMapCoordinate.getYAsInt();
 
