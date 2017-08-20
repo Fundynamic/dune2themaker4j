ae4359d8274a38de4f93f2f1f79e41b866f32e54
Stefan Hendriks <stefan@fundynamic.com>
2017-08-20 22:23:21 +0200

    When harvester is at a refinery, and in idle state, enter refinery

--- src/main/java/com/fundynamic/d2tm/game/entities/units/states/IdleHarvesterState.java
+++ src/main/java/com/fundynamic/d2tm/game/entities/units/states/IdleHarvesterState.java
@@ -1,6 +1,8 @@
 package com.fundynamic.d2tm.game.entities.units.states;
 
 
+import com.fundynamic.d2tm.game.entities.EntitiesSet;
+import com.fundynamic.d2tm.game.entities.Entity;
 import com.fundynamic.d2tm.game.entities.EntityRepository;
 import com.fundynamic.d2tm.game.entities.units.Unit;
 import com.fundynamic.d2tm.game.map.Map;
@@ -17,6 +19,15 @@ public class IdleHarvesterState extends UnitState {
         } else if (unit.canHarvest()) {
             unit.harvesting();
         } else {
+            EntitiesSet entitiesAt = entityRepository.findEntitiesAt(unit.getCoordinate());
+            EntitiesSet refineriesAtUnitLocation = entitiesAt.filterRefineries();
+            if (refineriesAtUnitLocation.hasAny()) {
+                // we have arrived at a refinery
+                Entity refinery = refineriesAtUnitLocation.getFirst();
+                unit.emptyHarvestedSpiceAt(refinery);
+                unit.enterOtherEntity(refinery);
+                return;
+            }
             // TODO: When seekspice fails, we should time this, so we won't run this code
             // every frame
             unit.seekSpice();
